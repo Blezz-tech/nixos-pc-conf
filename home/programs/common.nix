8846fff5ab1a7fa93b2ca66163d82c7bd4529381
blezz-tech <markus.jenya04@yandex.ru>
2024-05-01 23:26:41 +0300

    disable packages
    glow iotop iftop strace ltrace lsof sysstat lm_sensors ethtool

--- home/programs/common.nix
+++ home/programs/common.nix
@@ -172,21 +172,21 @@
 
     # productivity
     # hugo # static site generator
-    glow # markdown previewer in terminal
+    # glow # markdown previewer in terminal
 
     btop # replacement of htop/nmon
-    iotop # io monitoring
-    iftop # network monitoring
+    # iotop # io monitoring
+    # iftop # network monitoring
 
     # system call monitoring
-    strace # system call monitoring
-    ltrace # library call monitoring
-    lsof # list open files
+    # strace # system call monitoring
+    # ltrace # library call monitoring
+    # lsof # list open files
 
     # system tools
-    sysstat
-    lm_sensors # for `sensors` command
-    ethtool
+    # sysstat
+    # lm_sensors # for `sensors` command
+    # ethtool
     pciutils # lspci
     usbutils # lsusb
 
