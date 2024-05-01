9fe2db63e7eb6da12e2179f1dfe98813d2f6e00d
blezz-tech <markus.jenya04@yandex.ru>
2024-05-01 15:04:32 +0300

    вынос env в отдельный модуль

--- nixos/default.nix
+++ nixos/default.nix
@@ -165,8 +165,6 @@
     hack-font
   ];
 
-  environment.variables.EDITOR = "nvim";
-
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
--- nixos/modules/default.nix
+++ nixos/modules/default.nix
@@ -1,6 +1,7 @@
 {
   imports = [
     ./bootloader.nix
+    ./env
     ./sound.nix
   ];
 }
