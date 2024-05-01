a7b2df3f2889d56e3595365460dbf45f41e055fc
blezz-tech <markus.jenya04@yandex.ru>
2024-05-01 19:46:32 +0300

    add  enableNushellIntegration = true;

--- home/shell/starship.nix
+++ home/shell/starship.nix
@@ -3,6 +3,7 @@
   programs.starship = {
     enable = true;
     enableZshIntegration = true;
+    enableNushellIntegration = true;
     # custom settings
     settings = {
       add_newline = false;
