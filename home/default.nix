31c111c70b32936d49c49f78f40271a27208bf43
blezz-tech <markus.jenya04@yandex.ru>
2024-05-01 14:51:08 +0300

    refactoring

--- home/default.nix
+++ home/default.nix
@@ -10,19 +10,12 @@
     # ./rofi
   ];
 
-  home.username = "jenya";
-  home.homeDirectory = "/home/jenya";
-
-  # This value determines the home Manager release that your
-  # configuration is compatible with. This helps avoid breakage
-  # when a new home Manager release introduces backwards
-  # incompatible changes.
-  #
-  # You can update home Manager without changing this value. See
-  # the home Manager release notes for a list of state version
-  # changes in each release.
-  home.stateVersion = "23.11";
-
+  home = {
+    username = "jenya";
+    homeDirectory = "/home/jenya";
+    stateVersion = "23.11";
+  };
+  
   # Let home Manager install and manage itself.
   programs.home-manager.enable = true;
 }
