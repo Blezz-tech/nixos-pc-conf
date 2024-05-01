00b6f233db49ce2bf964cf0e3e8e02599b9d50e2
blezz-tech <markus.jenya04@yandex.ru>
2024-05-01 19:54:45 +0300

    add config for nushell

--- home/programs/nushell.nix
+++ home/programs/nushell.nix
@@ -5,5 +5,9 @@
   programs.nushell = {
     enable = true;
     package = pkgs.nushellFull;
+    extraConfig = ''
+      $env.PROMPT_COMMAND_RIGHT = ""
+      $env.config.show_banner = false
+    '';
   };
 }
