e5bb7ba8d15fbadc196484675c61bdcded015efa
blezz-tech <markus.jenya04@yandex.ru>
2024-05-01 17:13:21 +0300

    add flag for vivaldi

--- home/programs/browser.nix
+++ home/programs/browser.nix
@@ -5,7 +5,7 @@
     enable = true;
     package = pkgs.vivaldi;
     commandLineArgs = [
-      # "--disable-features=AllowQt --enable-blink-features=MiddleClickAutoscroll"
+      "--disable-features=AllowQt --enable-blink-features=MiddleClickAutoscroll"
       "--enable-blink-features=MiddleClickAutoscroll"
     ];
   };
