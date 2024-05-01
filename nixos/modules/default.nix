4b36ccac344723000f45f195d1219e818e7ff2d2
blezz-tech <markus.jenya04@yandex.ru>
2024-05-01 15:34:33 +0300

    объединение сервисов в один файл, тк.к они занимают 1-2 строчки

--- nixos/modules/default.nix
+++ nixos/modules/default.nix
@@ -1,16 +1,14 @@
 {
   imports = [
-    ./bootloader.nix
-    ./corectrl.nix
-    ./env.nix
-    ./nm.nix
     ./openssh.nix
     ./sound.nix
     ./steam.nix
     ./user.nix
-    ./virtmanager.nix
-    ./waydroid.nix
   ];
+
+  # bootloader
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
@@ -21,5 +19,19 @@
   # Менеджер разделов
   programs.partition-manager.enable = true;
 
+  # Сетевой менеждер
+  networking.networkmanager.enable = true;
   
+  # Переменные окружения
+  environment.variables.EDITOR = "nvim";
+
+  # Для ГПУ AMD 
+  programs.corectrl.enable = false;
+
+  # Менеджер виртуальный машин
+  virtualisation.libvirtd.enable = true;
+  programs.virt-manager.enable = true;
+
+  # Эмулятор Android
+  virtualisation.waydroid.enable = true;
 }
