01118aaf1aaa2420fcc3ac789862b1c0ca5cf942
blezz-tech <markus.jenya04@yandex.ru>
2024-05-02 00:58:41 +0300

    add packages for sway

--- home/sway/default.nix
+++ home/sway/default.nix
@@ -1,4 +1,14 @@
+{ pkgs
+, ...
+}:
 {
+  home.packages = with pkgs; [
+    grim # screenshot functionality
+    slurp # screenshot functionality
+    wl-clipboard # wl-copy and wl-paste for copy/paste from stdin / stdout
+    mako # notification system developed by swaywm maintainer
+  ];
+
   wayland.windowManager.sway = {
     enable = true;
     config = {
