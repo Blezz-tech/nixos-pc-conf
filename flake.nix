1c4783cb0303a67036a148015cab5195a5973a58
blezz-tech <markus.jenya04@yandex.ru>
2024-05-02 14:44:48 +0300

    refactoring

--- flake.nix
+++ flake.nix
@@ -4,10 +4,10 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+
 
     # nixvim = {
     #   url = "github:nix-community/nixvim";
