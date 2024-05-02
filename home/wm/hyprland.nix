b8108d58e419842bd5492c9ba77748bcacc0fac6
blezz-tech <markus.jenya04@yandex.ru>
2024-05-02 14:11:44 +0300

    enable any config hyprland

--- home/wm/hyprland.nix
+++ home/wm/hyprland.nix
@@ -82,52 +82,52 @@
         ];
       };
 
-      # dwindle = {
-      #   pseudotile = true; # master switch for pseudotiling. Enabling is bound to mainMod + P in the keybinds section below
-      #   preserve_split = true; # you probably want this
-      # };
+      dwindle = {
+        pseudotile = true; # master switch for pseudotiling. Enabling is bound to mainMod + P in the keybinds section below
+        preserve_split = true; # you probably want this
+      };
 
-      # master = {
-      #   new_is_master = true;
-      # };
+      master = {
+        new_is_master = true;
+      };
 
-      # gestures = {
-      #   workspace_swipe = true;
-      #   workspace_swipe_fingers = 3;
-      #   workspace_swipe_invert = false;
-      #   workspace_swipe_distance = 200;
-      #   workspace_swipe_forever = true;
-      # };
+      gestures = {
+        workspace_swipe = true;
+        workspace_swipe_fingers = 3;
+        workspace_swipe_invert = false;
+        workspace_swipe_distance = 200;
+        workspace_swipe_forever = true;
+      };
 
-      # misc = {
-      #   animate_manual_resizes = true;
-      #   animate_mouse_windowdragging = true;
-      #   enable_swallow = true;
-      #   render_ahead_of_time = false;
-      #   disable_hyprland_logo = true;
-      # };
+      misc = {
+        animate_manual_resizes = true;
+        animate_mouse_windowdragging = true;
+        enable_swallow = true;
+        render_ahead_of_time = false;
+        disable_hyprland_logo = true;
+      };
 
-      # windowrule = [
-      #   "float, ^(imv)$"
-      #   "float, ^(mpv)$"
-      # ];
+      windowrule = [
+        "float, ^(imv)$"
+        "float, ^(mpv)$"
+      ];
 
-      # exec-once = [
-      #   "swww init"
-      #   "swww img ~/Downloads/nixos-chan.png"
-      #   "waybar"
-      #   "wl-paste --type text --watch cliphist store"
-      #   "wl-paste --type image --watch cliphist store"
-      # ];
+      exec-once = [
+        "swww init"
+        "swww img ~/Downloads/nixos-chan.png"
+        "waybar"
+        "wl-paste --type text --watch cliphist store"
+        "wl-paste --type image --watch cliphist store"
+      ];
 
       bind = [
         #   "$mainMod, V, exec, cliphist list | wofi --dmenu | cliphist decode | wl-copy"
 
         "$mainMod, Return, exec, alacritty"
-        # "$mainMod, Q, killactive,"
+        "$mainMod, Q, killactive,"
         "$mainMod, M, exit,"
         "$mainMod, E, exec, dolphin"
-        # "$mainMod, F, togglefloating,"
+        "$mainMod, F, togglefloating,"
         # "$mainMod, D, exec, wofi --show drun"
         # "$mainMod, P, pseudo, # dwindle"
         # "$mainMod, J, togglesplit, # dwindle"
@@ -139,40 +139,40 @@
         "$mainMod, down,  movefocus, d"
 
         # Moving windows
-        # "$mainMod SHIFT, left,  swapwindow, l"
-        # "$mainMod SHIFT, right, swapwindow, r"
-        # "$mainMod SHIFT, up,    swapwindow, u"
-        # "$mainMod SHIFT, down,  swapwindow, d"
+        "$mainMod SHIFT, left,  swapwindow, l"
+        "$mainMod SHIFT, right, swapwindow, r"
+        "$mainMod SHIFT, up,    swapwindow, u"
+        "$mainMod SHIFT, down,  swapwindow, d"
 
         # Window resizing                     X  Y
-        # "$mainMod CTRL, left,  resizeactive, -60 0"
-        # "$mainMod CTRL, right, resizeactive,  60 0"
-        # "$mainMod CTRL, up,    resizeactive,  0 -60"
-        # "$mainMod CTRL, down,  resizeactive,  0  60"
+        "$mainMod CTRL, left,  resizeactive, -60 0"
+        "$mainMod CTRL, right, resizeactive,  60 0"
+        "$mainMod CTRL, up,    resizeactive,  0 -60"
+        "$mainMod CTRL, down,  resizeactive,  0  60"
 
         # Switch workspaces with mainMod + [0-9]
-        # "$mainMod, 1, workspace, 1"
-        # "$mainMod, 2, workspace, 2"
-        # "$mainMod, 3, workspace, 3"
-        # "$mainMod, 4, workspace, 4"
-        # "$mainMod, 5, workspace, 5"
-        # "$mainMod, 6, workspace, 6"
-        # "$mainMod, 7, workspace, 7"
-        # "$mainMod, 8, workspace, 8"
-        # "$mainMod, 9, workspace, 9"
-        # "$mainMod, 0, workspace, 10"
+        "$mainMod, 1, workspace, 1"
+        "$mainMod, 2, workspace, 2"
+        "$mainMod, 3, workspace, 3"
+        "$mainMod, 4, workspace, 4"
+        "$mainMod, 5, workspace, 5"
+        "$mainMod, 6, workspace, 6"
+        "$mainMod, 7, workspace, 7"
+        "$mainMod, 8, workspace, 8"
+        "$mainMod, 9, workspace, 9"
+        "$mainMod, 0, workspace, 10"
 
         # Move active window to a workspace with mainMod + SHIFT + [0-9]
-        # "$mainMod SHIFT, 1, movetoworkspacesilent, 1"
-        # "$mainMod SHIFT, 2, movetoworkspacesilent, 2"
-        # "$mainMod SHIFT, 3, movetoworkspacesilent, 3"
-        # "$mainMod SHIFT, 4, movetoworkspacesilent, 4"
-        # "$mainMod SHIFT, 5, movetoworkspacesilent, 5"
-        # "$mainMod SHIFT, 6, movetoworkspacesilent, 6"
-        # "$mainMod SHIFT, 7, movetoworkspacesilent, 7"
-        # "$mainMod SHIFT, 8, movetoworkspacesilent, 8"
-        # "$mainMod SHIFT, 9, movetoworkspacesilent, 9"
-        # "$mainMod SHIFT, 0, movetoworkspacesilent, 10"
+        "$mainMod SHIFT, 1, movetoworkspacesilent, 1"
+        "$mainMod SHIFT, 2, movetoworkspacesilent, 2"
+        "$mainMod SHIFT, 3, movetoworkspacesilent, 3"
+        "$mainMod SHIFT, 4, movetoworkspacesilent, 4"
+        "$mainMod SHIFT, 5, movetoworkspacesilent, 5"
+        "$mainMod SHIFT, 6, movetoworkspacesilent, 6"
+        "$mainMod SHIFT, 7, movetoworkspacesilent, 7"
+        "$mainMod SHIFT, 8, movetoworkspacesilent, 8"
+        "$mainMod SHIFT, 9, movetoworkspacesilent, 9"
+        "$mainMod SHIFT, 0, movetoworkspacesilent, 10"
 
         # Scroll through existing workspaces with mainMod + scroll
         # "$mainMod, mouse_down, workspace, e+1"
