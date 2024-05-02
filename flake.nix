9254cbd40181a8c3c832fb679ab8240d8d7a0587
blezz-tech <markus.jenya04@yandex.ru>
2024-05-02 14:52:08 +0300

    refactoring

--- flake.nix
+++ flake.nix
@@ -8,11 +8,8 @@
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
 
-
-    # nixvim = {
-    #   url = "github:nix-community/nixvim";
-    #   inputs.nixpkgs.follows = "nixpkgs";
-    # };
+    # nixvim.url = "github:nix-community/nixvim";
+    # nixvim.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = { nixpkgs, home-manager, ... }@inputs: {
