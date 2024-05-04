66e6a67043725fd3828f37e3db4a445e5736686e
blezz-tech <markus.jenya04@yandex.ru>
2024-05-04 22:04:09 +0300

    change aliases

--- home/shell/zsh.nix
+++ home/shell/zsh.nix
@@ -19,16 +19,17 @@
       # myViewConf = "cd myConf && nvim .";
       # myGit = "~/git-repos";
       # myServicesOption = "nixos-option services";
+
+      myErrors = "journalctl -p 3 -xb";
+
       myBuildSystem = "sudo nixos-rebuild switch --show-trace --flake ~/nixos-pc-conf#pc";
       myBuildHome = "home-manager switch --flake ~/nixos-pc-conf";
       myBuildAll = "myBuildSystem ; myBuildHome";
-      
-      myErrors = "journalctl -p 3 -xb";
-      myGenerations = "nix profile history --profile /nix/var/nix/profiles/system";
-      myDeleteGenerations = ''
-        sudo nix profile wipe-history --profile /nix/var/nix/profiles/system
-        nix profile wipe-history --profile ~/.local/state/nix/profiles/home-manager
-      '';
+
+      myDeleteSystem = "sudo nix profile wipe-history --profile /nix/var/nix/profiles/system";
+      myDeleteHome = "nix profile wipe-history --profile ~/.local/state/nix/profiles/home-manager";
+      myDeleteAll = "myDeleteSystem ; myDeleteHome";
+
       myClear = ''
         sudo nix store gc
         sudo nix store optimise
@@ -37,7 +38,7 @@
         sudo journalctl --rotate
         sudo journalctl --vacuum-time=1s
       '';
-      myNixbuild = "nix-build -E 'with import <nixpkgs> {}; callPackage ./default.nix {}' ";
+      # myNixbuild = "nix-build -E 'with import <nixpkgs> {}; callPackage ./default.nix {}' ";
       # myPasswordGenerator = "date +%s | sha256sum | base64 | head -c 32 ; echo";
       myPasswordGenerator = "pwgen -c 30";
 
