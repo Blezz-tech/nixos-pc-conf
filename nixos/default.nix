58c408d49b9f3a094b80881c5f88b612564592a2
blezz-tech <markus.jenya04@yandex.ru>
2024-05-25 23:33:16 +0300

    add options for nix

--- nixos/default.nix
+++ nixos/default.nix
@@ -11,7 +11,20 @@
 
   time.timeZone = "Europe/Moscow";
 
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix = {
+    optimise.automatic = true;
+
+    settings = {
+      experimental-features = [ "nix-command" "flakes" ];
+      auto-optimise-store = true;
+    };
+
+    gc = {
+      automatic = true;
+      dates = "daily";
+      options = "--delete-older-than 14d";
+    };
+  };
 
   system.stateVersion = "23.11";
 
