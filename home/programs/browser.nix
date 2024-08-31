8374ae5ce9ff78e95818a340dbc58879a8bba433
blezz-tech <markus.jenya04@yandex.ru>
2024-08-31 20:35:27 +0300

    Сборка vivaldi для plasma 6

--- home/programs/browser.nix
+++ home/programs/browser.nix
@@ -3,10 +3,18 @@
 }: {
   programs.chromium = {
     enable = true;
-    package = pkgs.vivaldi;
+    package = (pkgs.vivaldi.overrideAttrs (oldAttrs: {
+      dontWrapQtApps = false;
+      dontPatchELF = true;
+      nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [ pkgs.kdePackages.wrapQtAppsHook ];
+    }));
     commandLineArgs = [
-      "--disable-features=AllowQt"
+      # "--disable-features=AllowQt"
       "--enable-blink-features=MiddleClickAutoscroll"
+
+      "--enable-features=VaapiVideoDecodeLinuxGL"
+      "--ignore-gpu-blocklist"
+      "--enable-zero-copy"
     ];
   };
 }
