02920be027675fe5bc494b29d3d2c76968560239
blezz-tech <markus.jenya04@yandex.ru>
2024-08-31 23:27:43 +0300

    disable MURMUR_TOOLBOX

--- nixos/modules/work/qsoft.nix
+++ nixos/modules/work/qsoft.nix
@@ -13,18 +13,18 @@
     pkgs.nodejs_22
 
 
-    pkgs.MURMUR_TOOLBOX
+    # pkgs.MURMUR_TOOLBOX
   ];
 
-  nixpkgs.overlays = [
-    (self: super: {
-      MURMUR_TOOLBOX = super.jetbrains-toolbox.overrideAttrs (oldAttrs: {
-        src = self.fetchzip{
-          url = "https://github.com/Blezz-tech/MURMUR_PANEL/releases/download/other/MURMUR_TOOLBOX.tar.gz";
-          hash = "sha256-6sfO9tDIdp/xuNtqZ7UXqzP1SuLd6ZAF7lMTlaF3Z80=";
-          stripRoot = false;
-        };
-      });
-    })
-  ];
+  # nixpkgs.overlays = [
+  #   (self: super: {
+  #     MURMUR_TOOLBOX = super.jetbrains-toolbox.overrideAttrs (oldAttrs: {
+  #       src = self.fetchzip{
+  #         url = "https://github.com/Blezz-tech/MURMUR_PANEL/releases/download/other/MURMUR_TOOLBOX.tar.gz";
+  #         hash = "sha256-6sfO9tDIdp/xuNtqZ7UXqzP1SuLd6ZAF7lMTlaF3Z80=";
+  #         stripRoot = false;
+  #       };
+  #     });
+  #   })
+  # ];
 }
