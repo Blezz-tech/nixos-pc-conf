94d68a2c3ca3a2edc8abfe8e06b70fad832a363e
blezz-tech <markus.jenya04@yandex.ru>
2024-08-31 20:39:02 +0300

    Включение gnupg вместо openssh

--- nixos/modules/openssh.nix
+++ nixos/modules/openssh.nix
@@ -1,17 +1,17 @@
 {
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+  programs.mtr.enable = true;
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
 
   # Enable the OpenSSH daemon.
-  services.openssh = {
-    enable = true;
-    ports = [ 22 ];
-    settings.PasswordAuthentication = false;
-    settings.KbdInteractiveAuthentication = false;
-  };
+  # services.openssh = {
+  #   enable = true;
+  #   ports = [ 22 ];
+  #   settings.PasswordAuthentication = false;
+  #   settings.KbdInteractiveAuthentication = false;
+  # };
 }
