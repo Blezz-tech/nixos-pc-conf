7831bb63369e626217a542d3a83acce559c2547e
blezz-tech <markus.jenya04@yandex.ru>
2024-08-31 23:51:13 +0300

    Вещь для билда intellij-idea-ultimate

--- nixos/modules/work/qsoft.nix
+++ nixos/modules/work/qsoft.nix
@@ -11,7 +11,28 @@
 
   environment.systemPackages = [
     pkgs.nodejs_22
+    pkgs.jetbrains.idea-ultimate.overrideAttrs (oldAttrs: {
+      versions = {
+        x86_64-linux = {
+          idea-ultimate = {
 
+          };
+        };
+      };
+    })
+
+#     {
+#   "x86_64-linux": {
+#     "idea-ultimate": {
+#       "update-channel": "IntelliJ IDEA RELEASE",
+#       "url-template": "https://download.jetbrains.com/idea/ideaIU-{version}.tar.gz",
+#       "version": "2024.2.0.2",
+#       "sha256": "cf2159b9ea61ea910d27ad0e661cdd93575f65cdb443f99bd15600f5960b7f4b",
+#       "url": "https://drive.usercontent.google.com/download?id=1w-z0p-pMROW2xzPzRh9B6WsA_F0P7P3l&export=download&authuser=0&confirm=t&uuid=5f330f03-5987-4302-963f-2d599e096d35&at=AO7h07ddz2okNwDaLhjIAcy5_vtC:1725137225585",
+#       "build_number": "242.20224.419"
+#     }
+#   }
+# }
 
     # pkgs.MURMUR_TOOLBOX
   ];
