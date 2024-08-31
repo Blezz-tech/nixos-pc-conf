651916570ae3d46d0814bd432287ec31264455ac
blezz-tech <markus.jenya04@yandex.ru>
2024-08-31 23:27:05 +0300

    add MURMUR_TOOLBOX

--- nixos/modules/work/qsoft.nix
+++ nixos/modules/work/qsoft.nix
@@ -12,6 +12,19 @@
   environment.systemPackages = [
     pkgs.nodejs_22
 
-    # pkgs.jetbrains-toolbox
+
+    pkgs.MURMUR_TOOLBOX
+  ];
+
+  nixpkgs.overlays = [
+    (self: super: {
+      MURMUR_TOOLBOX = super.jetbrains-toolbox.overrideAttrs (oldAttrs: {
+        src = self.fetchzip{
+          url = "https://github.com/Blezz-tech/MURMUR_PANEL/releases/download/other/MURMUR_TOOLBOX.tar.gz";
+          hash = "sha256-6sfO9tDIdp/xuNtqZ7UXqzP1SuLd6ZAF7lMTlaF3Z80=";
+          stripRoot = false;
+        };
+      });
+    })
   ];
 }
