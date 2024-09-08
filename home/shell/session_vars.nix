a4d72c70a5f820d2fe3d0442e7a4fa9cab74faa1
blezz-tech <markus.jenya04@yandex.ru>
2024-09-08 10:26:21 +0300

    Добавление переменной среды для .cargo

--- home/shell/session_vars.nix
+++ home/shell/session_vars.nix
@@ -1,8 +1,8 @@
 {
   home.sessionVariables = {
-      # By default java does not enable antialiasing for font rendering. By exporting environment variables, this can be fixed:
-      _JAVA_OPTIONS = "-Dawt.useSystemAAFontSettings=lcd";
+    # By default java does not enable antialiasing for font rendering. By exporting environment variables, this can be fixed:
+    _JAVA_OPTIONS = "-Dawt.useSystemAAFontSettings=lcd";
 
-
+    CARGO_HOME = "~/.otherwise/.cargo";
   };
 }
