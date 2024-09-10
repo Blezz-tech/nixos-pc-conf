469a9d37df51b7b3968dc75200c548a9a7c26fa2
blezz-tech <markus.jenya04@yandex.ru>
2024-09-10 17:29:53 +0300

    Убран общесистемый прокси, добавлен флаг прокси для vivaldi

--- home/programs/browser.nix
+++ home/programs/browser.nix
@@ -14,6 +14,7 @@
       "--enable-blink-features=MiddleClickAutoscroll"
 
       "--enable-features=VaapiVideoDecodeLinuxGL"
+      "--proxy-server=""http://127.0.0.1:3999"""
       # "--ignore-gpu-blocklist"
       # "--enable-zero-copy"
     ];
