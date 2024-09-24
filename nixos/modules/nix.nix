d9dcef53edb629d90f38201c9d3946f6075487da
blezz-tech <markus.jenya04@yandex.ru>
2024-09-25 00:37:46 +0300

    Убраны параметры т.к. оптимизация пока не собирается

--- nixos/modules/nix.nix
+++ nixos/modules/nix.nix
@@ -7,6 +7,14 @@
         "nix-command"
         "flakes"
       ];
+      # system-features = [
+      #   "gccarch-alderlake"
+      #   "gcctune-alderlake"
+      #   "benchmark"
+      #   "big-parallel"
+      #   "kvm"
+      #   "nixos-test"
+      # ];
       auto-optimise-store = true;
     };
 
