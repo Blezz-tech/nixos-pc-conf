9f5c26307500e22f3dd612d3af954b197d65de7e
blezz-tech <markus.jenya04@yandex.ru>
2024-09-24 23:25:01 +0300

    Перемещение шрифтов в отдельный файл

--- nixos/packages.nix
+++ nixos/packages.nix
@@ -32,45 +32,4 @@
     # Диспеер раздеов дисков
     pkgs.partition-manager
   ];
-
-  fonts.packages = [
-    # Шрифты paratype
-    pkgs.paratype-pt-sans
-    pkgs.paratype-pt-mono
-    pkgs.paratype-pt-serif
-
-    # Шрифты terminus
-    pkgs.terminus_font
-
-    # Шрифты noto
-    pkgs.noto-fonts
-    pkgs.noto-fonts-lgc-plus
-    pkgs.noto-fonts-cjk
-    pkgs.noto-fonts-cjk-serif
-    pkgs.noto-fonts-cjk-sans
-    pkgs.noto-fonts-color-emoji
-    pkgs.noto-fonts-extra
-
-    # Шрифты windows
-    # pkgs.corefonts
-    pkgs.vistafonts
-
-    # Mono
-    pkgs.hack-font
-
-    # Остальные
-    pkgs.ibm-plex
-
-    pkgs.source-han-sans
-    pkgs.font-awesome
-    pkgs.source-code-pro
-    pkgs.source-sans
-    pkgs.source-sans-pro
-    pkgs.nerdfonts
-    pkgs.liberation_ttf
-    pkgs.scientifica
-
-    # A (mainly) Chinese Unicode font
-    pkgs.wqy_microhei
-  ];
 }
