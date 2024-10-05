9887bf3dd48683c4c979ecee7c69bca8078e80a7
blezz-tech <markus.jenya04@yandex.ru>
2024-10-05 11:37:58 +0300

    переменная среды CARGO_HOME изменена на соответствие стандарту XDG Base Directory

--- home/shell/session_vars.nix
+++ home/shell/session_vars.nix
@@ -3,6 +3,6 @@
     # By default java does not enable antialiasing for font rendering. By exporting environment variables, this can be fixed:
     _JAVA_OPTIONS = "-Dawt.useSystemAAFontSettings=lcd";
 
-    CARGO_HOME = "~/.otherwise/.cargo";
+    CARGO_HOME = ''"$XDG_DATA_HOME"/cargo'';
   };
 }
