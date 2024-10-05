cd541062f225afe3ab0867f2b32ccf2dc7d3dd02
blezz-tech <markus.jenya04@yandex.ru>
2024-10-05 13:08:36 +0300

    Включение стандартных переменных XDG

--- home/XDG/default.nix
+++ home/XDG/default.nix
@@ -1,21 +1,25 @@
 { config
 , ...
 }: {
-  xdg.userDirs = {
+  xdg = {
     enable = true;
-    createDirectories = true;
-    desktop = "${config.home.homeDirectory}/media/desktop";
-    download = "${config.home.homeDirectory}/downloads";
-    templates = "${config.home.homeDirectory}/media/templates";
-    publicShare = "${config.home.homeDirectory}/media/public";
-    documents = "${config.home.homeDirectory}/media/documents";
-    music = "${config.home.homeDirectory}/media/music";
-    pictures = "${config.home.homeDirectory}/media/images";
-    videos = "${config.home.homeDirectory}/media/videos";
 
-    extraConfig = {
-      XDG_PROJECTS_DIR = "${config.home.homeDirectory}/media/projects";
-      XDG_GAMES_DIR = "${config.home.homeDirectory}/media/games";
+    userDirs = {
+      enable = true;
+      createDirectories = true;
+      desktop = "${config.home.homeDirectory}/media/desktop";
+      download = "${config.home.homeDirectory}/downloads";
+      templates = "${config.home.homeDirectory}/media/templates";
+      publicShare = "${config.home.homeDirectory}/media/public";
+      documents = "${config.home.homeDirectory}/media/documents";
+      music = "${config.home.homeDirectory}/media/music";
+      pictures = "${config.home.homeDirectory}/media/images";
+      videos = "${config.home.homeDirectory}/media/videos";
+
+      extraConfig = {
+        XDG_PROJECTS_DIR = "${config.home.homeDirectory}/media/projects";
+        XDG_GAMES_DIR = "${config.home.homeDirectory}/media/games";
+      };
     };
   };
 }
