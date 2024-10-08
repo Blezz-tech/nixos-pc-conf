7c500c3fbba131a7ef98a4160b0032ad62c513d8
blezz-tech <markus.jenya04@yandex.ru>
2024-10-08 18:40:51 +0300

    Добавлен но отключён byedpi

--- nixos/services/default.nix
+++ nixos/services/default.nix
@@ -1,5 +1,13 @@
 {
   imports = [
+    # ./byedpi.nix
     # ./spoofdpi.nix
   ];
+
+  # services.byedpi = {
+  #   enable = true;
+  #   openFirewall = true;
+  #   socksPort = 3999;
+  #   commandLine = "--disorder 1 --auto=torst --tlsrec 1+s";
+  # };
 }
