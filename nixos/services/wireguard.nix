59133b169556341fb6a4a75fbbd0bbb4a9ef8923
blezz-tech <markus.jenya04@yandex.ru>
2024-10-09 23:17:21 +0300

    мурмур wireguard

--- nixos/services/wireguard.nix
+++ nixos/services/wireguard.nix
@@ -4,17 +4,18 @@
   };
   networking.wg-quick.interfaces = {
     wg0 = {
-      address = [ "10.8.0.82/24" ];
+      address = [ "10.8.0.2/24" ];
       dns = [ "1.1.1.1" ];
+      mtu = 1332;
       privateKeyFile = "/run/media/jenya/disk_d/vpn/SHOHOV.PrivateKey";
       
       peers = [
         {
-          publicKey = "0fhXUeR2Z1XiaNNvtOT40jZzDJs9FJstr7J+3lRcHTU=";
+          publicKey = "XEfBvGbnuBZ1vORNW65kjGScCX9UWmC6NF+Cjkmp5A0=";
           presharedKeyFile = "/run/media/jenya/disk_d/vpn/SHOHOV.PresharedKey";
           allowedIPs = [ "0.0.0.0/0" "::/0" ];
-          endpoint = "95.140.159.78:51820";
-          persistentKeepalive = 0;
+          endpoint = "195.58.38.51:2916";
+          persistentKeepalive = 25;
         }
       ];
     };
