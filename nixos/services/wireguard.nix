4f48157ef620539c6a7933575e1bd345b7560c36
blezz-tech <markus.jenya04@yandex.ru>
2024-10-09 23:22:32 +0300

    Добавление дурилки для РКН

--- nixos/services/wireguard.nix
+++ nixos/services/wireguard.nix
@@ -1,4 +1,6 @@
-{
+{ pkgs
+, ...
+}: {
   networking.firewall = {
     allowedUDPPorts = [ 51820 ]; # Clients and peers can use the same port, see listenport
   };
@@ -8,7 +10,8 @@
       dns = [ "1.1.1.1" ];
       mtu = 1332;
       privateKeyFile = "/run/media/jenya/disk_d/vpn/SHOHOV.PrivateKey";
-      
+      preUp = "ping --udp --count 1 --data-length 16 --source-port 2916 --dest-port 2916 195.58.38.51";
+
       peers = [
         {
           publicKey = "XEfBvGbnuBZ1vORNW65kjGScCX9UWmC6NF+Cjkmp5A0=";
@@ -20,4 +23,5 @@
       ];
     };
   };
+  environment.systemPackages = [ pkgs.nmap ];
 }
