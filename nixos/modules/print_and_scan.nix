a2d068b3f788ed9168d39f0e74c5aaa4a14cb51b
blezz-tech <markus.jenya04@yandex.ru>
2024-10-14 22:17:28 +0300

    delete specialisation for printing (use printing by default)

--- nixos/modules/print_and_scan.nix
+++ nixos/modules/print_and_scan.nix
@@ -1,17 +1,15 @@
 { pkgs
 , ...
 }: {
-  specialisation."with-printing-and-scanning".configuration = {
-    # Enable CUPS to print documents.
-    services.printing.enable = true;
-    services.printing.drivers = [ pkgs.samsung-unified-linux-driver ];
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+  services.printing.drivers = [ pkgs.samsung-unified-linux-driver ];
 
-    # Сканирование документов
-    hardware.sane.enable = true;
+  # Сканирование документов
+  hardware.sane.enable = true;
 
-    environment.systemPackages = [
-      pkgs.kdePackages.skanpage
-      pkgs.naps2
-    ];
-  };
+  environment.systemPackages = [
+    pkgs.kdePackages.skanpage
+    pkgs.naps2
+  ];
 }
