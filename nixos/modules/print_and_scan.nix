1f18e86f2b30e9296665223ce4a1a680073b73e4
blezz-tech <markus.jenya04@yandex.ru>
2024-10-27 14:54:59 +0300

    Добавление настроек для принтера и отключение их

--- nixos/modules/print_and_scan.nix
+++ nixos/modules/print_and_scan.nix
@@ -2,8 +2,22 @@
 , ...
 }: {
   # Enable CUPS to print documents.
-  services.printing.enable = true;
-  services.printing.drivers = [ pkgs.samsung-unified-linux-driver ];
+  services.printing = {
+    enable = true;
+    drivers = [ pkgs.samsung-unified-linux-driver ];
+    # openFirewall = true;
+    # defaultShared = true;
+  };
+
+  # services.avahi = {
+  #   enable = true;
+  #   nssmdns4 = true;
+  #   openFirewall = true;
+  #   publish = {
+  #     enable = true;
+  #     userServices = true;
+  #   };
+  # };
 
   # Сканирование документов
   hardware.sane.enable = true;
