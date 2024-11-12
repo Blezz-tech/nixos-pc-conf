a41607be113f8e99e4b5ab8f678a07f73ff32277
blezz-tech <markus.jenya04@yandex.ru>
2024-11-12 13:38:54 +0300

    update wireguard config

--- nixos/services/wireguard.nix
+++ nixos/services/wireguard.nix
@@ -3,23 +3,25 @@
 }: {
   networking.firewall = {
     allowedUDPPorts = [ 51820 ]; # Clients and peers can use the same port, see listenport
+    checkReversePath = false; 
   };
-  networking.wg-quick.interfaces = {
-    wg0 = {
-      address = [ "10.8.0.90/24" ];
-      dns = [ "1.1.1.1" ];
-      privateKeyFile = "/run/media/jenya/disk_d/vpn/vpn.PrivateKey";
+  # networking.wg-quick.interfaces.wg1.configFile = "/run/media/jenya/disk_d/vpn/wireguard.conf";
+  # networking.wg-quick.interfaces = {
+  #   wg0 = {
+  #     address = [ "10.8.0.90/24" ];
+  #     dns = [ "1.1.1.1" ];
+  #     privateKeyFile = "/run/media/jenya/disk_d/vpn/vpn.PrivateKey";
 
-      peers = [
-        {
-          publicKey = "bxG0yI+ML7ljhzFSoH/kp72tv8fukFAx23HXpD2nA00=";
-          presharedKeyFile = "/run/media/jenya/disk_d/vpn/vpn.PresharedKey";
-          allowedIPs = [ "0.0.0.0/0" "::/0" ];
-          endpoint = "217.25.95.90:51820";
-          persistentKeepalive = 0;
-        }
-      ];
-    };
-  };
+  #     peers = [
+  #       {
+  #         publicKey = "bxG0yI+ML7ljhzFSoH/kp72tv8fukFAx23HXpD2nA00=";
+  #         presharedKeyFile = "/run/media/jenya/disk_d/vpn/vpn.PresharedKey";
+  #         allowedIPs = [ "0.0.0.0/0" "::/0" ];
+  #         endpoint = "217.25.95.90:51820";
+  #         persistentKeepalive = 0;
+  #       }
+  #     ];
+  #   };
+  # };
   environment.systemPackages = [ pkgs.nmap ];
 }
