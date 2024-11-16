f998131fd2e723f7c3b8011edef3b1e85719cf74
blezz-tech <markus.jenya04@yandex.ru>
2024-11-16 21:49:34 +0300

    add git-cliff doe configuration

--- home/programs/git.nix
+++ home/programs/git.nix
@@ -2,33 +2,39 @@
 , ...
 }:
 {
-  programs.git = {
-    enable = true;
-    lfs.enable = true;
-    package = pkgs.gitFull;
+  programs = {
+    git = {
+      enable = true;
+      lfs.enable = true;
+      package = pkgs.gitFull;
 
-    userName = "blezz-tech";
-    userEmail = "markus.jenya04@yandex.ru";
-    signing.key = "78E9CCD7CF92CD8F";
-    extraConfig = {
-      init.defaultBranch = "master";
-      core.quotepath = false;
-      core.autocrlf = "input";
-      core.filemode = false;
-      commit.gpgsign = true;
-      tag.gpgsign = true;
-      credential.helper = "store";
-      # TODO: Нужно попробоавть использовать Git Credential Manager
-      # https://git-scm.com/book/en/v2/Git-Tools-Credential-Storage
+      userName = "blezz-tech";
+      userEmail = "markus.jenya04@yandex.ru";
+      signing.key = "78E9CCD7CF92CD8F";
+      extraConfig = {
+        init.defaultBranch = "master";
+        core.quotepath = false;
+        core.autocrlf = "input";
+        core.filemode = false;
+        commit.gpgsign = true;
+        tag.gpgsign = true;
+        credential.helper = "store";
+        # TODO: Нужно попробоавть использовать Git Credential Manager
+        # https://git-scm.com/book/en/v2/Git-Tools-Credential-Storage
 
-    };
+      };
 
-    ignores = [
+      ignores = [
         "venv"
         ".venv"
         ".env"
         "node_modules"
         ".direnv"
-    ];
+      ];
+    };
+
+    git-cliff = {
+      enable = true;
+    };
   };
 }
