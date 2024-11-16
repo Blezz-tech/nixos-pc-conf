b9952f207b438490057ac945e2e20a9c1607082c
blezz-tech <markus.jenya04@yandex.ru>
2024-11-16 21:44:30 +0300

    Добавление настроек для git

--- home/programs/git.nix
+++ home/programs/git.nix
@@ -11,6 +11,7 @@
     userEmail = "markus.jenya04@yandex.ru";
     signing.key = "78E9CCD7CF92CD8F";
     extraConfig = {
+      init.defaultBranch = "master";
       core.quotepath = false;
       core.autocrlf = "input";
       core.filemode = false;
@@ -21,5 +22,13 @@
       # https://git-scm.com/book/en/v2/Git-Tools-Credential-Storage
 
     };
+
+    ignores = [
+        "venv"
+        ".venv"
+        ".env"
+        "node_modules"
+        ".direnv"
+    ];
   };
 }
