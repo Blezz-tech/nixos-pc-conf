51406501019cedb7de7fa9b036e178416d88854d
blezz-tech <markus.jenya04@yandex.ru>
2024-11-19 17:44:49 +0300

    Отключение extraLocaleSettings

--- nixos/modules/i18n.nix
+++ nixos/modules/i18n.nix
@@ -1,18 +1,18 @@
 {
   i18n = {
-    supportedLocales = [ "all" ];
     defaultLocale = "ru_RU.UTF-8";
+    supportedLocales = [ "all" ];
 
-    extraLocaleSettings = {
-      LC_ADDRESS = "ru_RU.UTF-8";
-      LC_IDENTIFICATION = "ru_RU.UTF-8";
-      LC_MEASUREMENT = "ru_RU.UTF-8";
-      LC_MONETARY = "ru_RU.UTF-8";
-      LC_NAME = "ru_RU.UTF-8";
-      LC_NUMERIC = "ru_RU.UTF-8";
-      LC_PAPER = "ru_RU.UTF-8";
-      LC_TELEPHONE = "ru_RU.UTF-8";
-      LC_TIME = "ru_RU.UTF-8";
-    };
+    # extraLocaleSettings = {
+    #   LC_ADDRESS = "ru_RU.UTF-8";
+    #   LC_IDENTIFICATION = "ru_RU.UTF-8";
+    #   LC_MEASUREMENT = "ru_RU.UTF-8";
+    #   LC_MONETARY = "ru_RU.UTF-8";
+    #   LC_NAME = "ru_RU.UTF-8";
+    #   LC_NUMERIC = "ru_RU.UTF-8";
+    #   LC_PAPER = "ru_RU.UTF-8";
+    #   LC_TELEPHONE = "ru_RU.UTF-8";
+    #   LC_TIME = "ru_RU.UTF-8";
+    # };
   };
 }
