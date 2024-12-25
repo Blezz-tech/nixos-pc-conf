a0608fd3e8b08ffff11f66ce67355077e0877bfe
blezz-tech <markus.jenya04@yandex.ru>
2024-12-26 00:46:19 +0300

    disable steam game scope sessions

--- nixos/modules/steam.nix
+++ nixos/modules/steam.nix
@@ -1,24 +1,19 @@
 {
   programs = {
-    gamescope = {
-      enable = true;
-      capSysNice = true;
-    };
+    # gamescope = {
+    #   enable = true;
+    #   capSysNice = true;
+    # };
 
     steam = {
       enable = true;
 
       # Enable Game Scope
-      gamescopeSession.enable = true;
+      # gamescopeSession.enable = true;
 
-      # Open ports in the firewall for Steam Remote Play
-      remotePlay.openFirewall = true;
-
-      # Open ports in the firewall for Source Dedicated Server
-      dedicatedServer.openFirewall = true;
-
-      # Open ports in the firewall for Steam Local Network Game Transfers
-      localNetworkGameTransfers.openFirewall = true;
+      remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+      dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+      localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
     };
   };
 }
