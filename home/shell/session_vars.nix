4e91fcbb3e7521f29813f0965577fa0ab83ad3ad
blezz-tech <markus.jenya04@yandex.ru>
2025-01-04 13:15:18 +0300

    рефакторинг

--- home/shell/session_vars.nix
+++ home/shell/session_vars.nix
@@ -12,13 +12,21 @@
     _JAVA_OPTIONS = "-Dawt.useSystemAAFontSettings=lcd";
 
     # Исправление пути для приложений на соответствие со стандартом XDG Base Directory
+
+    ## XDG_DATA_HOME
     CARGO_HOME = "$XDG_DATA_HOME/cargo"; # ~/.cargo
-    XCOMPOSEFILE = "$XDG_CONFIG_HOME/X11/xcompose"; # ~/.XCompose
-    XCOMPOSECACHE = "$XDG_CACHE_HOME/X11/xcompose"; # ~/.compose-cache
-    NPM_CONFIG_USERCONFIG = "$XDG_CONFIG_HOME/npm/npmrc"; # for npm
     NODE_REPL_HISTORY = "$XDG_DATA_HOME/node_repl_history"; # ~/.node_repl_history
     REDISCLI_HISTFILE = "$XDG_DATA_HOME/redis/rediscli_history"; # ~/.rediscli_history
-    REDISCLI_RCFILE = "$XDG_CONFIG_HOME/redis/redisclirc"; # ~/.redisclirc 
+
+    ## XDG_CONFIG_HOME
+    XCOMPOSEFILE = "$XDG_CONFIG_HOME/X11/xcompose"; # ~/.XCompose
+    NPM_CONFIG_USERCONFIG = "$XDG_CONFIG_HOME/npm/npmrc"; # for npm
+    REDISCLI_RCFILE = "$XDG_CONFIG_HOME/redis/redisclirc"; # ~/.redisclirc
+
+    ## XDG_CACHE_HOME
+    XCOMPOSECACHE = "$XDG_CACHE_HOME/X11/xcompose"; # ~/.compose-cache
+
+    ## XDG_STATE_HOME
     PYTHON_HISTORY="$XDG_STATE_HOME/python/history"; # ~/.python_history
   };
 
