04f924a6c3ee0743e5645a20475821a49e7f4391
blezz-tech <markus.jenya04@yandex.ru>
2025-01-07 14:29:58 +0300

    disable unused VA-API config

--- nixos/hardware-configuration.nix
+++ nixos/hardware-configuration.nix
@@ -100,13 +100,6 @@
     # extraPackages32 = [
     #   pkgs.driversi686Linux.amdvlk
     # ];
-
-    # extraPackages = with pkgs; [
-    #   intel-media-driver # LIBVA_DRIVER_NAME=iHD
-    #   vaapiIntel # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
-    #   vaapiVdpau
-    #   libvdpau-va-gl
-    # ];
   };
 
   # environment.variables = {
