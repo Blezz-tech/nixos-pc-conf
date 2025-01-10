7fd9e8020041673a1838c15d755a004ea43423fa
blezz-tech <markus.jenya04@yandex.ru>
2025-01-10 10:28:02 +0300

    move commandLineArgs to overriding args

--- home/programs/browser.nix
+++ home/programs/browser.nix
@@ -8,17 +8,18 @@
       dontPatchELF = true;
       nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [ pkgs.kdePackages.wrapQtAppsHook ];
       # version = "6.9.3447.41";
+
+      commandLineArgs = [
+        # "--disable-features=AllowQt"
+        "--enable-blink-features=MiddleClickAutoscroll"
+
+        # "--proxy-server=http://127.0.0.1:3999"
+        # "--proxy-server=http://45.82.15.225:17200"
+
+        "--enable-features=VaapiVideoDecodeLinuxGL"
+        "--ignore-gpu-blocklist"
+        "--enable-zero-copy"
+      ];
     }));
-    commandLineArgs = [
-      # "--disable-features=AllowQt"
-      "--enable-blink-features=MiddleClickAutoscroll"
-
-      # "--proxy-server=http://127.0.0.1:3999"
-      # "--proxy-server=http://45.82.15.225:17200"
-
-      "--enable-features=VaapiVideoDecodeLinuxGL"
-      "--ignore-gpu-blocklist"
-      "--enable-zero-copy"
-    ];
   };
 }
