9d423832a3403a86270997228345a9b61df351ff
blezz-tech <markus.jenya04@yandex.ru>
2025-01-15 15:35:38 +0300

    add program: kde connect

--- nixos/programs/default.nix
+++ nixos/programs/default.nix
@@ -2,6 +2,7 @@
   imports = [
     ./chromium.nix
     ./firefox.nix
+    ./kdeconnect.nix
     # ./nvf.nix
     ./steam.nix
   ];
---
+++ nixos/programs/kdeconnect.nix
@@ -0,0 +1,10 @@
+{ pkgs
+, lib
+, ...
+}:
+{
+  programs.kdeconnect = {
+    enable = true;
+    package = lib.mkDefault pkgs.kdePackages.kdeconnect-kde;
+  };
+}
