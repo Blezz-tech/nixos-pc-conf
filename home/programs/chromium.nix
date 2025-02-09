9790b333859d8ce3f45897a98cf198b0d633e0f1
blezz-tech <markus.jenya04@yandex.ru>
2025-02-09 21:32:41 +0300

    delete disabled flags vivaldi

--- home/programs/chromium.nix
+++ home/programs/chromium.nix
@@ -10,11 +10,9 @@
       # version = "6.9.3447.41";
 
       commandLineArgs = [
-        # "--disable-features=AllowQt"
         "--enable-blink-features=MiddleClickAutoscroll"
 
         # "--proxy-server=http://127.0.0.1:3999"
-        # "--proxy-server=http://45.82.15.225:17200"
 
         "--use-gl=angle"
         "--use-angle=vulkan"
