c271bee1c13e92d65783755b8e0050bcd6687b5f
blezz-tech <markus.jenya04@yandex.ru>
2025-03-02 15:20:59 +0300

    remove plymouth

--- nixos/hardware-configuration.nix
+++ nixos/hardware-configuration.nix
@@ -18,36 +18,6 @@
     kernelModules = [ "kvm-intel" ];
     kernelPackages = pkgs.linuxPackages_latest;
     extraModulePackages = [ ];
-
-    # <nixos-wiki>
-    plymouth = {
-      enable = true;
-      theme = "target_2";
-      themePackages = with pkgs; [
-        # By default we would install all themes
-        (adi1090x-plymouth-themes.override {
-          selected_themes = [ "target_2" ];
-        })
-      ];
-    };
-
-    # Enable "Silent Boot"
-    consoleLogLevel = 0;
-    initrd.verbose = false;
-    kernelParams = [
-      "quiet"
-      "splash"
-      "boot.shell_on_fail"
-      "loglevel=3"
-      "rd.systemd.show_status=false"
-      "rd.udev.log_level=3"
-      "udev.log_priority=3"
-    ];
-    # Hide the OS choice for bootloaders.
-    # It's still possible to open the bootloader list by pressing any key
-    # It will just not appear on screen unless a key is pressed
-    loader.timeout = 0;
-    # </nixos-wiki>
   };
 
   fileSystems."/" = {
