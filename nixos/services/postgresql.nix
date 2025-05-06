e93cc0525c8cfbdf3df93a2820d4a3a7dec11db4
blezz-tech <markus.jenya04@yandex.ru>
2025-05-06 11:32:49 +0300

    add auth conf for postgresql

--- nixos/services/postgresql.nix
+++ nixos/services/postgresql.nix
@@ -1,6 +1,17 @@
-{
+{ lib
+, pkgs
+, ...
+}: {
   services.postgresql = {
     enable = true;
     enableJIT = true;
+    # TODO: Попробовать сделать auth-method = scram-sha-256
+    authentication = lib.mkOverride 10 ''
+      #type database  DBuser  auth-method
+      local all       all     trust
+    
+      #type database  DBuser  ipv4         auth-method
+      host  all       all     127.0.0.1/32 trust
+    '';
   };
 }
