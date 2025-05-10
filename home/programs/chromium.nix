4765c49ed1106aead561b0e41a7078edf5b23e3d
blezz-tech <markus.jenya04@yandex.ru>
2025-05-10 12:19:22 +0300

    delete vivaldi flags

--- home/programs/chromium.nix
+++ home/programs/chromium.nix
@@ -14,25 +14,7 @@
     --enable-features=Vulkan,VulkanFromANGLE,DefaultANGLEVulkan,VaapiIgnoreDriverChecks,VaapiVideoDecoder,AcceleratedVideoEncoder,UseMultiPlaneFormatForHardwareVideo,PlatformHEVCDecoderSupport,UseOzonePlatform
 
     # For Fcitx5
-    --enable-wayland-ime
+    --enable-wayland-ime=true
     --wayland-text-input-version=3
-  '';
-
-  xdg.configFile."vivaldi-stable.conf".text = ''
-    "--enable-blink-features=MiddleClickAutoscroll"
-
-    # "--proxy-server=http://127.0.0.1:3999"
-
-    # "--use-gl=angle"
-    # "--use-angle=vulkan"
-    "--ignore-gpu-blocklist"
-    # "--enable-gpu-rasterization"
-    "--enable-zero-copy"
-    # "--enable-accelerated-video-decode"
-    # "--enable-features=Vulkan,VulkanFromANGLE,DefaultANGLEVulkan,VaapiIgnoreDriverChecks,VaapiVideoDecoder,AcceleratedVideoEncoder,UseMultiPlaneFormatForHardwareVideo,PlatformHEVCDecoderSupport"
-
-    # For Fcitx5
-    # "--enable-wayland-ime"
-    # "--wayland-text-input-version=3"
   '';
 }
