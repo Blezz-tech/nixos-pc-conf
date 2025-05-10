f5341eb20fa24f4cd4126bd08d6567c7df91e369
blezz-tech <markus.jenya04@yandex.ru>
2025-05-10 12:19:01 +0300

    enable lsp

--- nixos/programs/nvf.nix
+++ nixos/programs/nvf.nix
@@ -11,12 +11,13 @@
         style = "dark";
       };
 
+      lsp.enable = true;
+
       statusline.lualine.enable = true;
       telescope.enable = true;
       autocomplete.nvim-cmp.enable = true;
 
       languages = {
-        enableLSP = true;
         enableTreesitter = true;
 
         nix.enable = true;
