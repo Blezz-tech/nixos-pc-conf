a2e47b9ca54f1b7f8c01d6e28ab165eaed6a30a7
blezz-tech <markus.jenya04@yandex.ru>
2025-05-15 22:05:05 +0300

    move MURMUR_TOOLBOX to overlays

--- nixos/default.nix
+++ nixos/default.nix
@@ -107,6 +107,13 @@
             }));
           };
         };
+        # MURMUR_TOOLBOX = super.jetbrains-toolbox.overrideAttrs (oldAttrs: {
+        #   src = self.fetchzip {
+        #     url = "https://github.com/Blezz-tech/MURMUR_PANEL/releases/download/other/MURMUR_TOOLBOX.tar.gz";
+        #     hash = "sha256-6sfO9tDIdp/xuNtqZ7UXqzP1SuLd6ZAF7lMTlaF3Z80=";
+        #     stripRoot = false;
+        #   };
+        # });
       };
     })
   ];
