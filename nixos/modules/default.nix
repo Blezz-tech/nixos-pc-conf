78df0e07ddc4599fdd8fd56bb397aa621bdd4fa1
blezz-tech <markus.jenya04@yandex.ru>
2025-05-25 23:32:26 +0300

    move programs for files

--- nixos/modules/default.nix
+++ nixos/modules/default.nix
@@ -23,18 +23,10 @@
     efi.canTouchEfiVariables = true;
   };
 
-  # Чтобы не было ошибок
-  programs.dconf.enable = true;
-
-  # Менеджер разделов
-  programs.partition-manager.enable = true;
-
   # Переменные окружения
   environment.variables.EDITOR = "nvim";
   environment.sessionVariables.NIXOS_OZONE_WL = "1"; # wayland для electron приложений
 
-  # Для ГПУ AMD 
-  programs.corectrl.enable = true;
 
   # Менеджер виртуальный машин
   virtualisation.libvirtd.enable = true;
@@ -43,31 +35,13 @@
   # Эмулятор Android
   # virtualisation.waydroid.enable = true;
 
-  # nix helper
-  programs.nh.enable = true;
-  programs.nh.flake = "/home/jenya/nixos-pc-conf/";
-
   # nixpkgs settings
   nixpkgs.config.allowUnfree = true;
   nixpkgs.config.permittedInsecurePackages = [ ];
   # nixpkgs.config.rocmSupport = true;
 
-  # kmail
-  # programs.kde-pim = {
-  #   # kmail = true;
-  #   kontact = true;
-  #   merkuro = true;
-  # };
-
   # Enable touchpad support (enabled default in most desktopManager).
   # libinput.enable = true;
-
-  # appimage-run
-  programs.appimage.enable = true;
-  programs.appimage.binfmt = true;
-
-  # openvpn client
-  # programs.openvpn3.enable = true;
 
   # MIME TYPE
   # xdg.mime.enable = true;
--- nixos/programs/default.nix
+++ nixos/programs/default.nix
@@ -10,4 +10,31 @@
   ];
 
   programs.zoom-us.enable = true;
+
+  # Чтобы не было ошибок
+  programs.dconf.enable = true;
+
+  # Менеджер разделов
+  programs.partition-manager.enable = true;
+
+  # Для ГПУ AMD 
+  programs.corectrl.enable = true;
+
+  # nix helper
+  programs.nh.enable = true;
+  programs.nh.flake = "/home/jenya/nixos-pc-conf/";
+
+  # kmail
+  # programs.kde-pim = {
+  #   # kmail = true;
+  #   kontact = true;
+  #   merkuro = true;
+  # };
+
+  # appimage-run
+  programs.appimage.enable = true;
+  programs.appimage.binfmt = true;
+
+  # openvpn client
+  # programs.openvpn3.enable = true;
 }
