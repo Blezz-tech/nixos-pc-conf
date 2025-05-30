4f26795a15b4b915ad4fc1b0e471147729338c16
blezz-tech <markus.jenya04@yandex.ru>
2025-05-30 17:30:34 +0300

    disable pkgs.blezz-pkgs.MURMUR.PHP.v2025_1_1

--- home/programs/common.nix
+++ home/programs/common.nix
@@ -51,7 +51,7 @@ in
   home.packages = [
     pkgs.blezz-pkgs.discord-krisp-patcher
     pkgs.blezz-pkgs.davinci-resolve
-    pkgs.blezz-pkgs.MURMUR.PHP.v2025_1_1
+    # pkgs.blezz-pkgs.MURMUR.PHP.v2025_1_1
     myPHP.php
     myPHP.php.packages.composer
     myPHP.php.packages.php-codesniffer
