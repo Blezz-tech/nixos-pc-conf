7dabea696340d44b8f55833c422871feea746d8a
blezz-tech <markus.jenya04@yandex.ru>
2025-07-19 22:19:25 +0300

    add subtituters keys

--- nixos/modules/nix.nix
+++ nixos/modules/nix.nix
@@ -3,12 +3,14 @@
     optimise.automatic = true;
 
     settings = {
-      # trusted-public-keys = [
-      #   "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=" # haskell.nix
-      # ];
-      # substituters = [
-      #   "https://cache.iog.io" # haskell.nix
-      # ];
+      trusted-public-keys = [
+        # "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=" # haskell.nix
+        "ghc-nix.cachix.org-1:ziC/I4BPqeA4VbtOFpFpu6D1t6ymFvRWke/lc2+qjcg=" # GHC
+      ];
+      substituters = [
+        # "https://cache.iog.io" # haskell.nix
+        "https://ghc-nix.cachix.org" # GHC
+      ];
 
       experimental-features = [
         "nix-command"
