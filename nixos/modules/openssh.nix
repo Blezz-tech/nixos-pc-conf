b08c830dc3f4038b53c9eb51b7549bca8fd98a1c
blezz-tech <markus.jenya04@yandex.ru>
2025-07-27 17:20:27 +0300

    add settings for gpg

--- nixos/modules/openssh.nix
+++ nixos/modules/openssh.nix
@@ -2,10 +2,13 @@
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   programs.mtr.enable = true;
-  programs.gnupg.agent = {
-    enable = true;
-    enableSSHSupport = true;
-    enableBrowserSocket = true;
+  programs.gnupg = {
+    agent = {
+      enable = true;
+      enableSSHSupport = true;
+      enableBrowserSocket = true;
+    };
+    dirmngr.enable = true;
   };
 
   # Enable the OpenSSH daemon.
